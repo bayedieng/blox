01922ac41c81d73da72147197b6c362653da3beb
Baye Dieng <bayedieng98@gmail.com>
2022-05-11 19:22:34 +0000

    start blox menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-use blox::lexer::{Lexer, TokenKind};
+use std::{env, process::exit};
+use std::io::{stdin, stdout,Write};
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        repl()
+    }
+    else if args.len() == 2 {
+        run_file(&args[1])
+    }
+    exit(0)
+
+}
+
+fn repl() {
+    loop {
+        print!("> ");
+        let mut s = String::new();
+        let _ = stdout().flush();
+        stdin().read_line(&mut s).unwrap();
+        print!("{s}")
+    }
+}
+
+fn run_file(_path: &str) {
+    
+}
